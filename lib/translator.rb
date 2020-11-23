6bc9767e484dc8928eb746cabd7336efc5ee51b5
Kat Leight <leightkt@gmail.com>
2020-11-23 22:10:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,8 +19,6 @@ def get_japanese_emoticon(file_path, emoticon)
   end
   return "Sorry, that emoticon was not found"
       emoticons[key][:japanese]
-    end
-  end
 end
 
 def get_english_meaning(file_path, emoticon)
