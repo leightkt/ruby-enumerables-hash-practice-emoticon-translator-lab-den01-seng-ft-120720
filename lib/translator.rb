cbd0ca45db93dcd3eafde7459740dc50916f7549
Kat Leight <leightkt@gmail.com>
2020-11-23 22:07:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,16 +14,14 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticons = load_library(file_path)
   emoticons.each do |key, value|
     if emoticons[key][:english] == emoticon
-<<<<<<< HEAD
       return emoticons[key][:japanese]
     end
   end
   return "Sorry, that emoticon was not found"
-=======
       emoticons[key][:japanese]
     end
   end
->>>>>>> 1250aa67810f2643028945e59c7cbf3d562a6f38
+
 end
 
 def get_english_meaning(file_path, emoticon)
