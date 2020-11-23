81142dd824e544a6f27c8b4fcc83af5ebed76b6c
Kat Leight <leightkt@gmail.com>
2020-11-23 22:05:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,10 +14,16 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticons = load_library(file_path)
   emoticons.each do |key, value|
     if emoticons[key][:english] == emoticon
+<<<<<<< HEAD
       return emoticons[key][:japanese]
     end
   end
   return "Sorry, that emoticon was not found"
+=======
+      emoticons[key][:japanese]
+    end
+  end
+>>>>>>> 1250aa67810f2643028945e59c7cbf3d562a6f38
 end
 
 def get_english_meaning(file_path, emoticon)
@@ -25,8 +31,17 @@ def get_english_meaning(file_path, emoticon)
   emoticons = load_library(file_path)
   emoticons.each do |key, value|
     if emoticons[key][:japanese] == emoticon
+<<<<<<< HEAD
       return key
     end
   end
   return "Sorry, that emoticon was not found"
 end
+=======
+      true
+    end
+  end
+end
+
+get_english_meaning('lib/emoticons.yml', "☜(⌒▽⌒)☞")
+>>>>>>> 1250aa67810f2643028945e59c7cbf3d562a6f38
